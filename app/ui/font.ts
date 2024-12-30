4df77d98993c95d59ed66ce21500b5588bc2a78b
John Bennett <john.bennett@prismhr.com>
2024-12-30 15:52:56 -0600

    initial commit

---
+++ app/ui/font.ts
@@ -0,0 +1,19 @@
+﻿import { Orbitron } from 'next/font/google';
+import { Lusitana } from "next/font/google";
+import { Ubuntu } from "next/font/google";
+
+export const orbitron = Orbitron({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-orbitron',
+});
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+})
+
+export const ubuntu = Ubuntu({
+    subsets: ['latin'],
+    weight: ['400', '700']
+})
